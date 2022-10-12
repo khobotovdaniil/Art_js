7e97f3e91b41d5ed28cdf6359b56abd9abf850e8
Daniil_Khobotov <dinich44@gmail.com>
2022-10-12 10:50:56 +0400

    add pictureSize

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import checkTextInputs from  './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import pictureSize from './modules/pictuteSize';
 
 window.addEventListener ('DOMContentLoaded', () => {
     "use strict";
@@ -20,5 +21,6 @@ window.addEventListener ('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
 
 });
